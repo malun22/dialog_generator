93c0149c266bd74a0a4d159c871785aa1f49c5a4
malun22 <malun22@web.de>
2023-10-19 12:05:11 +0200

    Extended colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,12 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        navbar: "#55a0b9",
+        "navbar-hover": "#daecf0",
+        "navbar-unactive": "#cdd9e1",
+        dark: "#454647",
+      },
     },
   },
   plugins: [],
