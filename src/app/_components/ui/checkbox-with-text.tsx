dbeb2c8c6a333e3efeeb4f8e0b2800cd88b2fe7e
malun22 <malun22@web.de>
2023-10-19 14:05:11 +0200

    Createed

---
+++ src/app/_components/ui/checkbox-with-text.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Checkbox } from "@/app/_components/ui/checkbox";
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+
+type CheckboxWithTextProps = {
+  id: string;
+  label: string;
+  description?: string;
+};
+
+const CheckboxWithText = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> &
+    CheckboxWithTextProps
+>(({ id, label, description, ...props }, ref) => (
+  <div className="items-top flex space-x-2">
+    <Checkbox {...props} ref={ref} id={id} />
+    <div className="grid gap-1.5 leading-none">
+      <label
+        htmlFor={id}
+        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+      >
+        {label}
+      </label>
+      {description && (
+        <p className="text-muted-foreground text-sm">{description}</p>
+      )}
+    </div>
+  </div>
+));
+
+export { CheckboxWithText };
