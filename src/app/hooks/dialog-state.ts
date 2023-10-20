ce35a4dded6ae83baaef40aa748736353bfcee0c
malun22 <malun22@web.de>
2023-10-20 10:37:41 +0200

    Added name

--- src/app/hooks/dialog-state.ts
+++ src/app/hooks/dialog-state.ts
@@ -1,14 +1,35 @@
 import { create } from "zustand";
+import { DialogElementProps } from "../_components/dialog-element";
 
 type DialogState = {
-  maxWidth: number;
+  name: string;
+  width: number;
+  height: number;
   showDefaultButtons: boolean;
-  // Takes a boolean return nothing
   setShowDefaultButtons: (to: boolean) => void;
+  setName: (to: string) => void;
+  elements: DialogElementProps[];
 };
 
 export const useDialogState = create<DialogState>((set) => ({
-  maxWidth: 500,
+  name: "Dialog",
+  width: 39,
+  height: 1,
   showDefaultButtons: true,
   setShowDefaultButtons: (to) => set({ showDefaultButtons: to }),
+  setName: (to) => set({ name: to }),
+  elements: [
+    {
+      x: 20,
+      y: 0,
+    },
+    {
+      x: 4,
+      y: 1,
+    },
+    {
+      x: 4,
+      y: 2,
+    },
+  ],
 }));
